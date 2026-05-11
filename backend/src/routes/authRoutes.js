1abd7ff21545a707931a9a82bb9a65e1ca02778b
Fidelis Agba <phyd3lis@yandex.com>
2026-05-11 22:31:36 +0100

    profile patch fix

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -14,6 +14,7 @@ import {
   loginController,
   refreshTokenController,
   getProfileController,
+  updateProfileController,
   changePasswordController,
   forceChangePasswordController,
   logoutController,
